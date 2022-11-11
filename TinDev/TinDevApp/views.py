f97f75afef7c85ebed24941faee5bc37b33db30c
Tyler Stinson <tstinson@nd.edu>
2022-11-10 20:06:56 -0500

    merged

--- TinDev/TinDevApp/views.py
+++ TinDev/TinDevApp/views.py
@@ -4,7 +4,5 @@ from django.contrib.auth.forms import UserCreationForm
 def loginPage(request):
     return render(request, 'TinDevApp/login.html')
 
-
-
 def new_user(request):
     return render(request, 'TinDevApp/user_create.html')
